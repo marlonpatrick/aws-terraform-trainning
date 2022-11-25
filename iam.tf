c4d8b2ce12811523bb5ca417c8c0983dba40448c
Marlon Patrick <marlonpatrick10@gmail.com>
2022-11-25 02:44:14 +0000

    Creating IAM Group

--- iam.tf
+++ iam.tf
@@ -17,12 +17,22 @@ resource "aws_iam_policy" "list_all_my_buckets" {
   policy = data.aws_iam_policy_document.list_all_my_buckets.json
 }
 
-resource "aws_iam_user" "created_by_terraform" {
-  name = "created_by_terraform"
+resource "aws_iam_group" "group_created_by_trf" {
+  name = "Group Created By Terraform"
+  path = "/"
+}
+
+resource "aws_iam_user" "user_created_by_trf" {
+  name = "user_created_by_terraform"
+}
+
+resource "aws_iam_user_group_membership" "training_membership" {
+  user   = aws_iam_user.user_created_by_trf.name
+  groups = [aws_iam_group.group_created_by_trf.name]
 }
 
 resource "aws_iam_user_policy_attachment" "training_attachment" {
-  user       = aws_iam_user.created_by_terraform.name
+  user       = aws_iam_user.user_created_by_trf.name
   policy_arn = aws_iam_policy.list_all_my_buckets.arn
 }
 
